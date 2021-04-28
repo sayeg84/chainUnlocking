1551bae7fd6beb957cb922fad7b3219a06f51b0a
choforito84 <sayeg@ciencias.unam.mx>
2021-04-28 11:45:58 -0500

    Added saving of table

--- src/lSimul.jl
+++ src/lSimul.jl
@@ -1,7 +1,13 @@
 include("io.jl")
+using Plots, DelimitedFiles
 
 function main()    
     ls,ts_mean,ts_error,rmsds_mean,rmsds_error = readLSimulation(ARGS[1])
+    println("saving results")
+    open(string(ARGS[1],"results.csv"),"w+") do io
+        table = hcat(ls,ts_mean,ts_error,rmsds_mean,rmsds_error)
+        DelimitedFiles.writedlm(io,table,',')
+    end
     println("Making Plots")
     scatter(ls,ts_mean,yerror=ts_error,label=false,title="L analysis",ylabel="steps",xlabel="L")
     savefig(string(ARGS[1],"l-t.pdf"))
@@ -11,6 +17,5 @@ function main()
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__
-    using Plots
     main()
 end
