fac75d1939b4692aa689bbbcd12b3b6a87ee842d
choforito84 <sayeg_84@hotmail.com>
2022-04-14 20:20:55 -0500

    Corrected bug of single simulation saving

--- src/runSingle.jl
+++ src/runSingle.jl
@@ -9,8 +9,9 @@ function main()
     if !isdir(parsed_args["path"])
         mkdir(parsed_args["path"])
     end
+    saveMetaParams(parsed_args["path"],simulation,parsed_args)
     P = chainFunc(parsed_args["lmin"])
-    runSingle(P,SimulType,parsed_args["path"],parsed_args)
+    runSingle(P,SimulType,joinpath(parsed_args["path"],""),parsed_args)
 end
 
 println("Single simulator")
