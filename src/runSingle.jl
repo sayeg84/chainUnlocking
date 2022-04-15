bd37a3e2bf391db4b62bab0053ff51944c9d4e6b
choforito84 <sayeg_84@hotmail.com>
2022-04-14 20:24:55 -0500

    Corrected bug

--- src/runSingle.jl
+++ src/runSingle.jl
@@ -9,7 +9,7 @@ function main()
     if !isdir(parsed_args["path"])
         mkdir(parsed_args["path"])
     end
-    saveMetaParams(parsed_args["path"],simulation,parsed_args)
+    saveMetaParams(parsed_args["path"],SimulType,parsed_args)
     P = chainFunc(parsed_args["lmin"])
     runSingle(P,SimulType,joinpath(parsed_args["path"],""),parsed_args)
 end
