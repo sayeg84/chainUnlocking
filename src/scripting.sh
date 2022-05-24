a5be8bf7c198d99be836fbfee0c7934e5a6bd826
sayeg84 <sayeg@ciencias.unam.mx>
2022-05-24 08:24:56 +0000

    Add new posibilities to scripting

--- src/scripting.sh
+++ src/scripting.sh
@@ -1,6 +1,9 @@
-for folder in fourKnotLsAnnealingSpan/ fourKnotLsAnnealingSpanRefin/ fourKnotLsAnnealingTangent/ fourKnotLsAnnealingTangentRefin/ fourKnotLsGeneticSpan/ fourKnotLsGeneticTangent/ needleLsAnnealingSpan/ needleLsAnnealingTangent/ needleLsGeneticSpan/ needleLsGeneticTangent/ 
+#for folder in fourKnotLsAnnealingSpan/ fourKnotLsAnnealingSpanRefin/ fourKnotLsAnnealingTangent/ fourKnotLsAnnealingTangentRefin/ fourKnotLsGeneticSpan/ fourKnotLsGeneticTangent/ needleLsAnnealingSpan/ needleLsAnnealingTangent/ needleLsGeneticSpan/ needleLsGeneticTangent/
+for folder in fourKnotLsAnnealingTangent/ fourKnotLsAnnealingTangentRefin/ fourKnotLsGeneticTangent/ needleLsAnnealingTangent/  needleLsGeneticTangent 
 do
-    #julia lsAnalysis.jl --path ../outouts/$folder --burnout 0.95 
-    ls ../outouts/$folder/metaParams.txt
-    echo "done"
+    julia lsAnalysisDistributed.jl --path ../outputs/$folder --burnout 0.95 --name  max_span_minimum --minFunc squaredMaxSpan  --processes 40 --minimum
+    #cat ../outputs/${folder}/metaParams.txt
+    echo ""
+	echo "done"
+    echo ""
 done
